70a847578c1f1743550747e9beaa391bf6971e1e
Carrosen <carla1.rosen@gmail.com>
2019-04-29 19:03:00 +0200

    adds it block to spec about if no game rules are met, 1/1 going green

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,24 +1,11 @@
 const { Game } = require('../spec.helper')
 
-// describe('Game', () => {
-//     let game = new Game
-//     let Player1 = ('value1').value
-//     let Player2 = ('value2').value
-
-//     // it('returns a string if no game rules are met', () => {
-//     //     expect(game.check("bajs")).to.eql('That is not the game pal, try "rock", "paper" or "scissors"')
-//     // })
-
-//     it('returns Its a tie if Player1 === Player2', () => {
-//         expect(game.check(Player1 === Player2)).to.eql("Its a tie!")
-//     })
-// })
-
-
 describe('Game', () => {
     let game = new Game
 
-
+        it('returns a string if no game rules are met', () => {
+            expect(game.check("bajs")).to.eql('That is not the game pal, try "rock", "paper" or "scissors"')
+        })
 
      describe('Player1 wins', () => {
 
