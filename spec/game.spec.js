eeb07af9a1897280b63d92d2f74b402da8b645e1
Carrosen <carla1.rosen@gmail.com>
2019-07-02 17:00:45 +0200

    Code formatting and changes image

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,66 +1,54 @@
 const { Game } = require('../spec.helper')
 
 describe('Game', () => {
-    let game = new Game
+  let game = new Game
 
-        it('returns a string if no game rules are met', () => {
-            expect(game.check("bajs")).to.eql('That is not the game pal, try "rock", "paper" or "scissors"')
-        })
+  it('returns a string if no game rules are met', () => {
+    expect(game.check("bajs")).to.eql('That is not the game pal, try "rock", "paper" or "scissors"')
+  })
 
-     describe('Player1 wins', () => {
+  describe('Player1 wins', () => {
+    it('returns Player1 is the winner if Player1 enters "rock"', () => {
+      expect(game.check('rock', 'scissors')).to.eql('Player1 is the winner!')
+    })
 
-        it('returns Player1 is the winner if Player1 enters "rock"', () => {
-            expect(game.check('rock', 'scissors')).to.eql('Player1 is the winner!')
-        })
+    it('returns Player1 is the winner if Player1 enters "paper"', () => {
+      expect(game.check('paper', 'rock')).to.eql('Player1 is the winner!')
+    })
 
-        it('returns Player1 is the winner if Player1 enters "paper"', () => {
-            expect(game.check('paper', 'rock')).to.eql('Player1 is the winner!')
-        })
+    it('returns Player1 is the winner if Player1 enters "scissors"', () => {
+      expect(game.check('scissors', 'paper')).to.eql('Player1 is the winner!')
+    })
 
-        it('returns Player1 is the winner if Player1 enters "scissors"', () => {
-            expect(game.check('scissors', 'paper')).to.eql('Player1 is the winner!')
-        })
+  })
 
+  describe('Player2 wins', () => {
+    it('returns Player2 is the winner if Player2 enters "rock"', () => {
+      expect(game.check('scissors', 'rock')).to.eql('Player2 is the winner!')
+    })
+
+    it('returns Player2 is the winner if Player2 enters "paper"', () => {
+      expect(game.check('rock', 'paper')).to.eql('Player2 is the winner!')
+    })
+
+    it('returns Player2 is the winner if Player2 enters "scissors"', () => {
+      expect(game.check('paper', 'scissors')).to.eql('Player2 is the winner!')
     })
 
 
+  })
 
-    
-      describe('Player2 wins', () => {
-
-        it('returns Player2 is the winner if Player2 enters "rock"', () => {
-            expect(game.check('scissors', 'rock')).to.eql('Player2 is the winner!')
-        })
-
-        it('returns Player2 is the winner if Player2 enters "paper"', () => {
-            expect(game.check('rock', 'paper')).to.eql('Player2 is the winner!')
-        })
-
-        it('returns Player2 is the winner if Player2 enters "scissors"', () => {
-            expect(game.check('paper', 'scissors')).to.eql('Player2 is the winner!')
-        })
-
-
+  describe('Its a tie!', () => {
+    it('returns Its a tie! if Player1 enters "rock"', () => {
+      expect(game.check('rock', 'rock')).to.eql("Its a tie!")
     })
 
-
-    describe('Its a tie!', () => {
-    
-        it('returns Its a tie! if Player1 enters "rock"', () => {
-            expect(game.check('rock', 'rock')).to.eql("Its a tie!")
-        })
-
-        it('returns Its a tie! if Player1 enters "paper"', () => {
-            expect(game.check('paper', 'paper')).to.eql("Its a tie!")
-        })
-
-        it('returns Its a tie! if Player1 enters "scissors"', () => {
-            expect(game.check('scissors', 'scissors')).to.eql("Its a tie!")
-        })
+    it('returns Its a tie! if Player1 enters "paper"', () => {
+      expect(game.check('paper', 'paper')).to.eql("Its a tie!")
     })
 
+    it('returns Its a tie! if Player1 enters "scissors"', () => {
+      expect(game.check('scissors', 'scissors')).to.eql("Its a tie!")
+    })
+  })
 })
-
-    //     it('returns Its a tie! if Player1 === Player2', () => {
-    //         expect(game.check(Player1 === Player2)).to.eql("Its a tie!")
-    //     })
--- src/js/game.js
+++ src/js/game.js
@@ -1,30 +1,30 @@
 function Game() {
-    this.check = (Player1, Player2) => {
+  this.check = (Player1, Player2) => {
 
-        if (Player1 === Player2) {
-            return 'Its a tie!';
-        } 
-    
-        else if ((Player1 === "rock") && (Player2 === "scissors")) {
-            return 'Player1 is the winner!';
-        }
-        else if ((Player1 === "rock") && (Player2 === "paper")) {
-            return 'Player2 is the winner!';
-        }
-        else if ((Player1 === "paper") && (Player2 === "rock")) {
-            return 'Player1 is the winner!';
-        }
-        else if ((Player1 === "paper") && (Player2 === "scissors")) {
-            return 'Player2 is the winner!';
-        }
-        else if ((Player1 === "scissors") && (Player2 === "paper")) {
-            return 'Player1 is the winner!';
-        }
-        else if ((Player1 === "scissors") && (Player2 === "rock")) {
-            return 'Player2 is the winner!';
-        }
-        else {
-            return 'That is not the game pal, try "rock", "paper" or "scissors"';
-        }
+    if (Player1 === Player2) {
+      return 'Its a tie!';
+    } 
+
+    else if ((Player1 === "rock") && (Player2 === "scissors")) {
+      return 'Player1 is the winner!';
     }
+    else if ((Player1 === "rock") && (Player2 === "paper")) {
+      return 'Player2 is the winner!';
+    }
+    else if ((Player1 === "paper") && (Player2 === "rock")) {
+      return 'Player1 is the winner!';
+    }
+    else if ((Player1 === "paper") && (Player2 === "scissors")) {
+      return 'Player2 is the winner!';
+    }
+    else if ((Player1 === "scissors") && (Player2 === "paper")) {
+      return 'Player1 is the winner!';
+    }
+    else if ((Player1 === "scissors") && (Player2 === "rock")) {
+      return 'Player2 is the winner!';
+    }
+    else {
+      return 'That is not the game pal, try "rock", "paper" or "scissors"';
+    }
+  }
 }
